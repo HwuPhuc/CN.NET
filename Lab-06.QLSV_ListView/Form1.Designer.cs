d9990b6d08e69376f2cd07eb554d773ecfe5bcb7
HwuPhuc <161864409+HwuPhuc@users.noreply.github.com>
2026-03-05 20:50:02 +0700

    Add files via upload

---
+++ Lab-06.QLSV_ListView/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Lab_06.QuanLySinhVien
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtMaSV = new TextBox();
+            label2 = new Label();
+            txtHoTen = new TextBox();
+            btLuu = new Button();
+            btClear = new Button();
+            btClose = new Button();
+            label3 = new Label();
+            btSearch = new Button();
+            txtTimKiem = new TextBox();
+            label4 = new Label();
+            btChon1 = new Button();
+            btTrave1 = new Button();
+            btTraveDS = new Button();
+            btChonDS = new Button();
+            lvDanhSachSV = new ListView();
+            columnSTT = new ColumnHeader();
+            columnMaSV = new ColumnHeader();
+            columnHoten = new ColumnHeader();
+            lvDachon = new ListView();
+            colummSTT2 = new ColumnHeader();
+            columnMasv2 = new ColumnHeader();
+            columnHoten2 = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(62, 40);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Mã SV";
+            // 
+            // txtMaSV
+            // 
+            txtMaSV.Location = new Point(15, 78);
+            txtMaSV.Margin = new Padding(5, 5, 5, 5);
+            txtMaSV.Name = "txtMaSV";
+            txtMaSV.Size = new Size(222, 39);
+            txtMaSV.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(340, 34);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 32);
+            label2.TabIndex = 2;
+            label2.Text = "Họ tên";
+            // 
+            // txtHoTen
+            // 
+            txtHoTen.Location = new Point(284, 77);
+            txtHoTen.Margin = new Padding(5, 5, 5, 5);
+            txtHoTen.Name = "txtHoTen";
+            txtHoTen.Size = new Size(324, 39);
+            txtHoTen.TabIndex = 3;
+            // 
+            // btLuu
+            // 
+            btLuu.Location = new Point(673, 54);
+            btLuu.Margin = new Padding(5, 5, 5, 5);
+            btLuu.Name = "btLuu";
+            btLuu.Size = new Size(177, 74);
+            btLuu.TabIndex = 4;
+            btLuu.Text = "Thêm";
+            btLuu.UseVisualStyleBackColor = true;
+            btLuu.Click += btLuu_Click;
+            // 
+            // btClear
+            // 
+            btClear.Location = new Point(881, 54);
+            btClear.Margin = new Padding(5, 5, 5, 5);
+            btClear.Name = "btClear";
+            btClear.Size = new Size(177, 74);
+            btClear.TabIndex = 5;
+            btClear.Text = "Xóa";
+            btClear.UseVisualStyleBackColor = true;
+            btClear.Click += btClear_Click;
+            // 
+            // btClose
+            // 
+            btClose.Location = new Point(1103, 54);
+            btClose.Margin = new Padding(5, 5, 5, 5);
+            btClose.Name = "btClose";
+            btClose.Size = new Size(177, 74);
+            btClose.TabIndex = 6;
+            btClose.Text = "Thoát";
+            btClose.UseVisualStyleBackColor = true;
+            btClose.Click += btClose_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(21, 157);
+            label3.Margin = new Padding(5, 0, 5, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(228, 32);
+            label3.TabIndex = 7;
+            label3.Text = "Danh sách sinh viên";
+            // 
+            // btSearch
+            // 
+            btSearch.Location = new Point(270, 142);
+            btSearch.Margin = new Padding(5, 5, 5, 5);
+            btSearch.Name = "btSearch";
+            btSearch.Size = new Size(148, 48);
+            btSearch.TabIndex = 9;
+            btSearch.Text = "Tìm";
+            btSearch.UseVisualStyleBackColor = true;
+            btSearch.Click += btSearch_Click;
+            // 
+            // txtTimKiem
+            // 
+            txtTimKiem.Location = new Point(436, 147);
+            txtTimKiem.Margin = new Padding(5, 5, 5, 5);
+            txtTimKiem.Name = "txtTimKiem";
+            txtTimKiem.Size = new Size(322, 39);
+            txtTimKiem.TabIndex = 10;
+            txtTimKiem.TextChanged += txtTimKiem_TextChanged;
+            txtTimKiem.KeyPress += txtTimKiem_KeyPress;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(1048, 157);
+            label4.Margin = new Padding(5, 0, 5, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(218, 32);
+            label4.TabIndex = 11;
+            label4.Text = "Danh sách đã chọn";
+            // 
+            // btChon1
+            // 
+            btChon1.Location = new Point(570, 230);
+            btChon1.Margin = new Padding(5, 5, 5, 5);
+            btChon1.Name = "btChon1";
+            btChon1.Size = new Size(138, 77);
+            btChon1.TabIndex = 14;
+            btChon1.Text = ">";
+            btChon1.UseVisualStyleBackColor = true;
+            btChon1.Click += btChon1_Click;
+            // 
+            // btTrave1
+            // 
+            btTrave1.Location = new Point(574, 322);
+            btTrave1.Margin = new Padding(5, 5, 5, 5);
+            btTrave1.Name = "btTrave1";
+            btTrave1.Size = new Size(138, 77);
+            btTrave1.TabIndex = 15;
+            btTrave1.Text = "<";
+            btTrave1.UseVisualStyleBackColor = true;
+            btTrave1.Click += btTrave1_Click;
+            // 
+            // btTraveDS
+            // 
+            btTraveDS.Location = new Point(574, 499);
+            btTraveDS.Margin = new Padding(5, 5, 5, 5);
+            btTraveDS.Name = "btTraveDS";
+            btTraveDS.Size = new Size(138, 77);
+            btTraveDS.TabIndex = 17;
+            btTraveDS.Text = "<<";
+            btTraveDS.UseVisualStyleBackColor = true;
+            btTraveDS.Click += btTraveDS_Click;
+            // 
+            // btChonDS
+            // 
+            btChonDS.Location = new Point(570, 408);
+            btChonDS.Margin = new Padding(5, 5, 5, 5);
+            btChonDS.Name = "btChonDS";
+            btChonDS.Size = new Size(138, 77);
+            btChonDS.TabIndex = 16;
+            btChonDS.Text = ">>";
+            btChonDS.UseVisualStyleBackColor = true;
+            btChonDS.Click += btChonDS_Click;
+            // 
+            // lvDanhSachSV
+            // 
+            lvDanhSachSV.Columns.AddRange(new ColumnHeader[] { columnSTT, columnMaSV, columnHoten });
+            lvDanhSachSV.FullRowSelect = true;
+            lvDanhSachSV.Location = new Point(18, 214);
+            lvDanhSachSV.Margin = new Padding(5, 5, 5, 5);
+            lvDanhSachSV.Name = "lvDanhSachSV";
+            lvDanhSachSV.Size = new Size(537, 487);
+            lvDanhSachSV.TabIndex = 18;
+            lvDanhSachSV.UseCompatibleStateImageBehavior = false;
+            lvDanhSachSV.View = View.Details;
+            // 
+            // columnSTT
+            // 
+            columnSTT.Text = "STT";
+            // 
+            // columnMaSV
+            // 
+            columnMaSV.Text = "Mã SV";
+            // 
+            // columnHoten
+            // 
+            columnHoten.Text = "Họ tên";
+            // 
+            // lvDachon
+            // 
+            lvDachon.Columns.AddRange(new ColumnHeader[] { colummSTT2, columnMasv2, columnHoten2 });
+            lvDachon.Location = new Point(741, 211);
+            lvDachon.Margin = new Padding(5, 5, 5, 5);
+            lvDachon.Name = "lvDachon";
+            lvDachon.Size = new Size(537, 487);
+            lvDachon.TabIndex = 19;
+            lvDachon.UseCompatibleStateImageBehavior = false;
+            lvDachon.View = View.Details;
+            // 
+            // colummSTT2
+            // 
+            colummSTT2.Text = "STT";
+            // 
+            // columnMasv2
+            // 
+            columnMasv2.Text = "Mã SV";
+            // 
+            // columnHoten2
+            // 
+            columnHoten2.Text = "Họ tên";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1300, 720);
+            Controls.Add(lvDachon);
+            Controls.Add(lvDanhSachSV);
+            Controls.Add(btTraveDS);
+            Controls.Add(btChonDS);
+            Controls.Add(btTrave1);
+            Controls.Add(btChon1);
+            Controls.Add(label4);
+            Controls.Add(txtTimKiem);
+            Controls.Add(btSearch);
+            Controls.Add(label3);
+            Controls.Add(btClose);
+            Controls.Add(btClear);
+            Controls.Add(btLuu);
+            Controls.Add(txtHoTen);
+            Controls.Add(label2);
+            Controls.Add(txtMaSV);
+            Controls.Add(label1);
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "Form1";
+            Text = "Quản lý sinh viên";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtMaSV;
+        private Label label2;
+        private TextBox txtHoTen;
+        private Button btLuu;
+        private Button btClear;
+        private Button btClose;
+        private Label label3;
+        private Button btSearch;
+        private TextBox txtTimKiem;
+        private Label label4;
+        private Button btChon1;
+        private Button btTrave1;
+        private Button btTraveDS;
+        private Button btChonDS;
+        private ListView lvDanhSachSV;
+        private ColumnHeader columnSTT;
+        private ColumnHeader columnMaSV;
+        private ColumnHeader columnHoten;
+        private ListView lvDachon;
+        private ColumnHeader colummSTT2;
+        private ColumnHeader columnMasv2;
+        private ColumnHeader columnHoten2;
+    }
+}
---
+++ Lab-06.QLSV_ListView/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_06.QuanLySinhVien
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool kiemTraMaSV(string ma)
+        {
+            for (int i = 0; i < lvDanhSachSV.Items.Count; i++)
+            {
+                if (lvDanhSachSV.Items[i].SubItems[1].Text == ma)
+                {
+                    return false;
+                }
+            }
+
+            for (int i = 0; i < lvDachon.Items.Count; i++)
+            {
+                if (lvDachon.Items[i].SubItems[1].Text == ma)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void tim(ListView lv, string text)
+        {
+            text = text.ToUpper();
+            for (int i = 0; i < lv.Items.Count; i++)
+            {
+                string maSV = lv.Items[i].SubItems[1].Text.ToUpper();
+                string hoTen = lv.Items[i].SubItems[2].Text.ToUpper();
+                if (maSV.Contains(text) || hoTen.Contains(text))
+                {
+                    lv.Items[i].Selected = true;
+                    lv.Items[i].EnsureVisible();
+                    lv.Items[i].Focused = true;
+                }
+                else
+                {
+                    lv.Items[i].Selected = false;
+                }
+
+            }
+        }
+
+        private void thucHienTim()
+        {
+            tim(lvDanhSachSV, txtTimKiem.Text);
+            txtTimKiem.SelectAll();
+            txtTimKiem.Focus();
+        }
+        private void txtTimKiem_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((Keys)e.KeyChar == Keys.Enter)
+            {
+                thucHienTim();
+            }
+        }
+        private void btSearch_Click(object sender, EventArgs e)
+        {
+            thucHienTim();
+        }
+
+        private void btLuu_Click(object sender, EventArgs e)
+        {
+            if (txtMaSV.Text.Trim() == "" || txtHoTen.Text.Trim() == "")
+            {
+                MessageBox.Show("Bạn chưa nhập đủ thông tin!", "Thông báo");
+            }
+            else if (kiemTraMaSV(txtMaSV.Text.Trim()) == false)
+            {
+                MessageBox.Show("Mã số sinh viên đã tồn tại, vui lòng nhập lại!", "Thông báo");
+                txtMaSV.SelectAll();
+                txtMaSV.Focus();
+            }
+            else
+            {
+                int stt = lvDanhSachSV.Items.Count + 1;
+
+                ListViewItem li = new ListViewItem(stt.ToString());
+                li.SubItems.Add(txtMaSV.Text.Trim());
+                li.SubItems.Add(txtHoTen.Text.Trim());
+
+                lvDanhSachSV.Items.Add(li);
+                txtMaSV.Text = "";
+                txtHoTen.Text = "";
+                txtMaSV.Focus();
+            }
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i < lvDanhSachSV.Items.Count)
+            {
+                if (lvDanhSachSV.Items[i].Selected == true)
+                {
+                    lvDanhSachSV.Items.RemoveAt(i);
+                }
+                else
+                {
+                    i++;
+                }
+                danhSoThuTu(lvDanhSachSV);
+            }
+        }
+
+        private void danhSoThuTu(ListView lv)
+        {
+            for (int i = 0; i < lv.Items.Count; i++)
+            {
+                lv.Items[i].Text = (i + 1).ToString();
+            }
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Bạn có thực sự muốn thoát không?", "Xác nhận",
+                                   MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dr == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                ListViewItem li = new ListViewItem(i.ToString());
+                li.SubItems.Add("SV" + i);
+                li.SubItems.Add("Nguyễn Văn " + i);
+
+                lvDanhSachSV.Items.Add(li);
+
+            }
+        }
+        private void txtTimKiem_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btChon1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i < lvDanhSachSV.Items.Count)
+            {
+                if (lvDanhSachSV.Items[i].Selected == true)
+                {
+                    ListViewItem li = lvDanhSachSV.Items[i];
+
+                    lvDanhSachSV.Items.RemoveAt(i);
+                    lvDachon.Items.Add(li);
+                }
+                else
+                {
+                    i++;
+                }
+                danhSoThuTu(lvDanhSachSV);
+            }
+        }
+
+        private void btTrave1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i < lvDachon.Items.Count)
+            {
+                if (lvDachon.Items[i].Selected == true)
+                {
+                    ListViewItem li = lvDachon.Items[i];
+
+                    lvDachon.Items.RemoveAt(i);
+                    lvDanhSachSV.Items.Add(li);
+                }
+                else
+                {
+                    i++;
+                }
+                danhSoThuTu(lvDachon);
+            }
+        }
+
+        private void btChonDS_Click(object sender, EventArgs e)
+        {
+            while (lvDanhSachSV.Items.Count > 0)
+            {
+                ListViewItem li = lvDanhSachSV.Items[0];
+
+                lvDanhSachSV.Items.RemoveAt(0);
+                lvDachon.Items.Add(li);
+            }
+        }
+
+        private void btTraveDS_Click(object sender, EventArgs e)
+        {
+            while (lvDachon.Items.Count > 0)
+            {
+                ListViewItem li = lvDachon.Items[0];
+
+                lvDachon.Items.RemoveAt(0);
+                lvDanhSachSV.Items.Add(li);
+            }
+        }
+    }
+}
